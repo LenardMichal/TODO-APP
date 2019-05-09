61b73eb9a2230aafa1efcc305df91c08ccc0e6d8
Michal <lenard.michal@yahoo.com>
2019-05-09 12:03:17 +0200

    Initial build subtree commit

---
+++ precache-manifest.d7fd1753818eb4fa1717524b46570640.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ca7de6fd4ebf3a3f161dc7e9e86567f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ea3f2668613ff0f235e",
+    "url": "/static/css/main.409625e5.chunk.css"
+  },
+  {
+    "revision": "111af06b279b52799cbc",
+    "url": "/static/js/2.9ae84df5.chunk.js"
+  },
+  {
+    "revision": "4ea3f2668613ff0f235e",
+    "url": "/static/js/main.3a73105b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
